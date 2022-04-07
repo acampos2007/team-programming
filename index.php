e448951025701a568aa690735b30365e00927529
Angel Campos <campos.angel@student.greenriver.edu>
2022-04-07 16:17:40 -0400

    updated index.php, created js folder, created cupcake.js, created process page

---
+++ index.php
@@ -0,0 +1,46 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Cupcake Form</title>
+</head>
+<body>
+
+<h2>Cupcake Fundraiser</h2>
+
+<fieldset>
+<form id="cupcake-form" action="process.php" method="get">
+    <p>Your Name:</p>
+    <input type="text" id="name" placeholder="Please enter your name" name="name">
+    <span class="err" id="err-name"></span>
+    <br>
+
+    <p>Cupcake Flavors</p>
+
+    <ul style="list-style-type: none">
+        <li><input type="checkbox" name="flavor[]" value="Chocolate"> Chocolate</li>
+        <li><input type="checkbox" name="flavor[]" value="Vanilla"> Vanilla</li>
+        <li><input type="checkbox" name="flavor[]" value="Tiramisu"> Tiramisu</li>
+        <li><input type="checkbox" name="flavor[]" value="Whiskey Maple Bacon"> Whiskey Maple Bacon</li>
+        <li><input type="checkbox" name="flavor[]" value="Red Velvet"> Red Velvet</li>
+        <li><input type="checkbox" name="flavor[]" value="Carrot Cake"> Carrot Cake</li>
+    </ul>
+    <span class="err" id="err-flavor"></span>
+
+    <button type="submit" id="submit">Order</button>
+</form>
+</fieldset>
+
+
+
+
+
+<?php
+//include("js/cupcake.js");
+?>
+</body>
+</html>
---
+++ process.php
@@ -0,0 +1,41 @@
+<?php
+
+
+/*function getFlavor()
+{
+    $flavor = array('Chocolate', 'Vanilla', 'Tiramisu', 'Whiskey Maple Bacon', 'Red Velvet', 'Carrot Cake');
+    return $flavor;
+}*/
+
+$name = $_GET['name'];
+$flavor = implode("<li>", $_GET['flavor']);
+
+define("Cupcake_Cost", 3.50);
+
+$numCupcakes = sizeof($_GET['flavor']);
+
+$price = 0.0;
+$price += $numCupcakes * Cupcake_Cost;
+//$price += $flavor * Cupcake_Cost;
+
+$price = number_format($price, 2);
+
+
+thanks($name);
+echo '<h3>Order Summary:</h3>';
+echo"<p><li>$flavor</li></p>";
+echo"<p>Order Total: $price</p>";
+
+function thanks($name = "")
+{
+    $msg = "<h1> Thank you, $name, for your order!";
+    /*if($name != "")
+    {
+        $msg .= ", $name";
+    }
+    $msg .= "!</h1>";*/
+    echo $msg;
+}
+
+
+?>
