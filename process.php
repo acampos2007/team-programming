ee50978801c7baea287be5f91c9e33aa78565fe6
Angel Campos <campos.angel@student.greenriver.edu>
2022-04-12 22:14:32 -0400

    updated process.php and style/cupcake.css

--- process.php
+++ process.php
@@ -8,6 +8,15 @@ Description: cupcake confirmation
     $flavor = array('Chocolate', 'Vanilla', 'Tiramisu', 'Whiskey Maple Bacon', 'Red Velvet', 'Carrot Cake');
     return $flavor;
 }*/
+$isValid = true;
+if(empty($_GET['name']))
+{
+    $isValid = false;
+}
+if(empty($_GET['flavor']))
+{
+    $isValid = false;
+}
 
 $name = $_GET['name'];
 $flavor = implode("<li>", $_GET['flavor']);
@@ -23,21 +32,29 @@ $price += $numCupcakes * Cupcake_Cost;
 $price = number_format($price, 2);
 
 
+
+if($isValid == false)
+{
+    echo "Please complete the order form.";
+}
+else
+{
+    function thanks($name = "")
+    {
+        $msg = "<h1> Thank you, $name, for your order!";
+        /*if($name != "")
+        {
+            $msg .= ", $name";
+        }
+        $msg .= "!</h1>";*/
+        echo $msg;
+    }
+}
+
 thanks($name);
 echo '<h3>Order Summary:</h3>';
 echo"<p><li>$flavor</li></p>";
 echo"<p>Order Total: $price</p>";
-
-function thanks($name = "")
-{
-    $msg = "<h1> Thank you, $name, for your order!";
-    /*if($name != "")
-    {
-        $msg .= ", $name";
-    }
-    $msg .= "!</h1>";*/
-    echo $msg;
-}
 
 
 ?>
